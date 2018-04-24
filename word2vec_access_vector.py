d70911e307e050db9a90bf1228778e96fdea061c
evigog <evangeliagog@gmail.com>
2018-04-24 13:43:12 +0200

    word2vec of data

--- word2vec_access_vector.py
+++ word2vec_access_vector.py
@@ -1,9 +1,33 @@
 import gensim
+from data_preprocessing import *
+import numpy as np
 
-# Load Google's pre-trained Word2Vec model.
-model = gensim.models.KeyedVectors.load_word2vec_format('GoogleNews-vectors-negative300.bin', binary=True)
+def data_word2vec():
+    data = load_data_and_labels('datasets/rt-polaritydata/rt-polarity.neg', 'datasets/rt-polaritydata/rt-polarity.pos')
+    sentences = data[0]
 
-vocab = model.vocab.keys()
+    # Load Google's pre-trained Word2Vec model.
+    model = gensim.models.KeyedVectors.load_word2vec_format('GoogleNews-vectors-negative300.bin', binary=True)
+    vocab_size = model.vector_size  #vector size is 300 (num of words used from google vocabulary)
 
-i = model.vocab["for"].index
-vector = model.vectors[i]
+
+    #generate word vector representation of our data
+    word_vectors = [] #list of all word2vec representations
+    for i in range(len(sentences)):
+        word_list = sentences[i].split(" ")
+        mat = np.zeros((len(word_list), vocab_size)) #mat representation of each sentence
+        for j in range(len(word_list)):
+            #check if word not found in dictionary
+            if (word_list[j] in model.vocab.keys()):
+                idx = int(model.vocab[word_list[j]].index)
+                mat[j,:] = model.vectors[idx]
+            else:
+                print('not valid word!')
+                mat[j,:] = -1 #not valid word vector
+
+        mat = mat[~np.all(mat == -1, axis=1)]  #delete non valid vectors
+        word_vectors.append(mat)
+
+    np.save('Google_Wordvec', word_vectors)
+
+    return word_vectors
