5a3e46e8d30f61e6e9329593e2a45aab0ab89eed
evigog <evangeliagog@gmail.com>
2018-04-27 17:29:24 +0200

    first cnn done

--- network.py
+++ network.py
@@ -15,14 +15,14 @@ def cnn_model_fn(features, labels, mode):
   # Input Layer
   dim_x = 46  #max sentence size
   dim_y = 300  #wordvec dimensions
-  input_layer = tf.reshape(features, [-1, dim_x, dim_y, 1]) #-1 corresponds to the batch, 1 corresponds to the channel used
+  input_layer = tf.reshape(features, [-1, dim_x, dim_y, 1]) #-1 corresponds to the batch (dynamicallly calculated), 1 corresponds to the channel used
 
   # Convolutional Layer - one for each filter size
   filter_sizes = [(3, dim_y), (4, dim_y), (5, dim_y)]
   pooled_output = []
 
   #define each group of filters
-  conv_output = []
+  layer_output = []
   for size in filter_sizes:
 
       conv = tf.layers.conv2d(
@@ -32,46 +32,30 @@ def cnn_model_fn(features, labels, mode):
           padding="same",
           activation=tf.nn.relu)
 
-      conv_output.append(conv)
+      # max over time pooling
+      pooling = tf.nn.max_pool(conv, ksize=[1,  46-size[0]+1, 1, 1],
+                                      strides=[1,1,1,1],
+                                      padding='VALID',
+                                      name="pool")
+
+      layer_output.append(pooling)
 
 
   #concatenate the filter output
-  concat_output = tf.concat(conv_output, 3)  #size=(100,46,300,300)
-
-  #max over time pooling
-  pooling_output = tf.nn.max_pool(concat_output, ksize=[1,46, 1, 1],
-                    strides=[1, 1, 1, 1],
-                    padding='VALID',
-                    name="pool")
+  concat_output = tf.concat(layer_output, 1)  #size=(100,3+4+5,300,100)
+  reshape_output = tf.reshape(concat_output, [-1,12*300*100])
 
 
- # Dense Layer,
-
-  dense = tf.layers.dense(inputs=pool2_flat, units=1024, activation=tf.nn.relu)
+ # Dense Layer for the dropout,
+  dense = tf.layers.dense(inputs=reshape_output, units=100, activation=tf.nn.relu)
   dropout = tf.layers.dropout(
       inputs=dense, rate=0.5, training=mode == tf.estimator.ModeKeys.TRAIN)  #dropout rate
 
   # Logits Layer
-  logits = tf.layers.dense(inputs=dropout, units=2)   #two classes (positive and negative)
+  logits = tf.layers.dense(inputs=dropout, units=2, activation=tf.nn.softmax)   #two classes (positive and negative)
 
-  if mode == tf.estimator.ModeKeys.PREDICT:
-    return tf.estimator.EstimatorSpec(mode=mode, predictions=predictions)
-
-  # Calculate Loss (for both TRAIN and EVAL modes)
-  loss = tf.losses.sparse_softmax_cross_entropy(labels=labels, logits=logits)
-
-  # Configure the Training Op (for TRAIN mode)
-  if mode == tf.estimator.ModeKeys.TRAIN:
-    optimizer = tf.train.GradientDescentOptimizer(learning_rate=0.001)
-    train_op = optimizer.minimize(
-        loss=loss,
-        global_step=tf.train.get_global_step())
-    return tf.estimator.EstimatorSpec(mode=mode, loss=loss, train_op=train_op)
-
-  # Add evaluation metrics (for EVAL mode)
-  eval_metric_ops = {
-      "accuracy": tf.metrics.accuracy(
-          labels=labels, predictions=predictions["classes"])}
+  b=logits.eval()
+  a=logits
 
   return tf.estimator.EstimatorSpec(
       mode=mode, loss=loss, eval_metric_ops=eval_metric_ops)
@@ -115,6 +99,7 @@ def main(unused_argv):
       num_epochs=1,
       shuffle=False)
   eval_results = mnist_classifier.evaluate(input_fn=eval_input_fn)
+
   print(eval_results)
 
 
