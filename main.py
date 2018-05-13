393142d8619c7b137ea9532ff001527b92936a70
pythonpsycho1337 <Thomas_pierre_elis@hotmail.com>
2018-05-13 14:15:05 +0000

    Bug fix needed

--- main.py
+++ main.py
@@ -3,18 +3,22 @@ from __future__ import division
 from __future__ import print_function
 
 import preprocessing.word2vec_access_vector as wordvec
+import sys
 from train import *
 from test import *
 
 tf.logging.set_verbosity(tf.logging.INFO)
 
-def main(dataset):
+def main():
+    dataset = sys.argv[1]
     if dataset=="MR":
         wordvecPath = "preprocessing/wordvectors_polarity/wordVecMR.npy"
         labelsPath = "preprocessing/wordvectors_polarity/labelsMR.npy"
     elif dataset=="Twitter":
         wordvecPath = "preprocessing/wordvectors_twitter/wordVecTwitter.npy"
         labelsPath = "preprocessing/wordvectors_twitter/labelsTwitter.npy"
+    else
+   	print("Could not find data for the dataset "+dataset)
     #dataset takes values "MR" or "Twitter"
     data = wordvec.load_data(wordvecPath,labelsPath)
     train_features = data[0][:100,:]
@@ -22,7 +26,7 @@ def main(dataset):
     test_features = data[2][:100,:]
     test_labels = data[3][:100]
 
-
+    print("succesfully loaded "+dataset+" dataset")
     train((train_features,train_labels), 'ckpt')
     test_network((test_features, test_labels), 'ckpt')
 
