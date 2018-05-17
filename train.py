ec77f02f9f0d8de82868a2c35f7523aa79ec5771
pythonpsycho1337 <Thomas_pierre_elis@hotmail.com>
2018-05-17 09:30:43 +0000

    Minor bugfix

--- train.py
+++ train.py
@@ -2,6 +2,7 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
+import numpy as np
 from network import *
 import tensorflow as tf
 from Parameters import *
