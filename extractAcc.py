c859d7649b3d589c9bfa95eb38381e59188ea6d7
pythonpsycho1337 <Thomas_pierre_elis@hotmail.com>
2018-05-21 18:45:14 +0000

    Added a file for extracting the validation accuracies

---
+++ extractAcc.py
@@ -0,0 +1,8 @@
+#Extracts validation and test accuracy from logs
+import pickle,sys
+path = sys.argv[1]
+f = open(path,"rb")
+log = pickle.load(f)
+
+for i in range(0,len(log)):
+	print(",".join([str(i) for i in log[i]["modelParams"]["FilterSizes"]])+" & "+str(100*log[i]["valAcc"])+"\%\\\\\\hline")
