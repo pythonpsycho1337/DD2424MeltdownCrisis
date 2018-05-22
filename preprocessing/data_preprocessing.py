28143afb1897d49c76237c642ea6a693bb23e7bb
pythonpsycho1337 <Thomas_pierre_elis@hotmail.com>
2018-05-22 11:34:09 +0200

    Removed duplicate clean_str function and load_sst since that one is not used

--- preprocessing/data_preprocessing.py
+++ preprocessing/data_preprocessing.py
@@ -6,27 +6,6 @@ import numpy as np
 import re
 
 # ---------- preprossesing of rt-polaritydata -----------#
-def clean_str(string):
-    """
-    Tokenization/string cleaning for all datasets except for SST.
-    Original taken from https://github.com/yoonkim/CNN_sentence/blob/master/process_data.py
-    """
-    string = re.sub(r"[^A-Za-z0-9(),!?\'\`\\)]", " ", string)
-    string = re.sub(r"\'s", " \'s", string)
-    string = re.sub(r"\'ve", " \'ve", string)
-    string = re.sub(r"n\'t", " n\'t", string)
-    string = re.sub(r"\'re", " \'re", string)
-    string = re.sub(r"\'d", " \'d", string)
-    string = re.sub(r"\'ll", " \'ll", string)
-    string = re.sub(r",", " , ", string)
-    string = re.sub(r"!", " ! ", string)
-    string = re.sub(r"\(", " \( ", string)
-    string = re.sub(r"\)", " \) ", string)
-    string = re.sub(r"\?", " \? ", string)
-    string = re.sub(r"\s{2,}", " ", string)
-    return string.strip().lower()
-
-
 def load_data_and_labels(positive_data_file, negative_data_file):
     """
     Loads MR polarity data from files, splits the data into words and generates labels.
@@ -78,28 +57,6 @@ def clean_str(string):
     string = re.sub('"', '', string)
 
     return string.strip().lower()
-
-
-def load_SST(sentencesFile,datasetSplitFile,labelsFile):
-    """
-    Loads SST polarity data from files
-    NOT DONE
-    """
-    # Load data from files
-    sentences = list(open(sentencesFile, "r").readlines())
-    datasetSplit = list(open(datasetSplitFile, "r").readlines())
-    labels = list(open(labelsFile, "r").readlines())
-
-    #sentences = [s.strip() for s in sentences] #Needed?
-
-    # Split by words
-    sentences = [clean_str(sentence) for sentence in sentences]
-
-    # Generate labels
-    #positive_labels = [[0, 1] for _ in positive_examples]
-    #negative_labels = [[1, 0] for _ in negative_examples]
-    #y = np.concatenate([positive_labels, negative_labels], 0)
-    #return [x_text, y]
 
 def load_twitter(fileName,includeNeutral=True):
     #Load the twitter data
